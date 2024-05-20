427ee929e9cce8c429de2ad024ba1abf77852e73
Andrew Sharpe <asharpe@squiz.net>
2024-05-20 18:58:44 +1000

    fixup for publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import terser from "@rollup/plugin-terser";
 import { either, isNil, isEmpty, reject } from "rambda";
 
@@ -13,6 +14,7 @@ const isDev = process.env.NODE_ENV === "development";
 const isNilOrEmpty = either(isNil, isEmpty);
 
 const plugins = reject(isNilOrEmpty)([
+  peerDepsExternal(),
   typescript(tsConfig),
   isDev ? undefined : terser(),
 ]);
@@ -20,7 +22,7 @@ const plugins = reject(isNilOrEmpty)([
 const commonConfig = {
   input: "src/index.ts",
   plugins,
-  external: ["zod", "path", "fs"],
+  external: ["dns", "spf-parse", "ip-regex", "rambda"],
 };
 
 export default [
@@ -30,13 +32,16 @@ export default [
       dir: "./dist",
       entryFileNames: "index.js",
       format: "cjs",
+      exports: "named",
     },
   },
 
   // ES
   {
     ...commonConfig,
-
-    output: { file: "dist/es/index.js", format: "esm" },
+    output: {
+		 file: "dist/es/index.js",
+		 format: "esm",
+	 },
   },
 ];
